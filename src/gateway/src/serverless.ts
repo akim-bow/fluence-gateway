a91d5d8fbacbe81b7f840bbd7ded854e4d2fc0f8
Akim Mamedov <akim99999999@gmail.com>
2024-02-13 01:34:40 +0700

    handler func

--- src/gateway/src/serverless.ts
+++ src/gateway/src/serverless.ts
@@ -1,5 +1,8 @@
 import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = fastify({
   logger: true,
@@ -7,7 +10,7 @@ const server = fastify({
 
 await server.register(import("./app/index.js"));
 
-export default async (req: Request, res: Response) => {
+export default async function handler(req: Request, res: Response) {
   await server.ready();
   server.server.emit("request", req, res);
-};
+}
