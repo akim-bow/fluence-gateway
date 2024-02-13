4e0877722d60433ebca3594b0d085d07a189898b
Akim Mamedov <akim99999999@gmail.com>
2024-02-13 16:28:30 +0700

    fix import attribute

--- src/gateway/api/serverless.ts
+++ src/gateway/api/serverless.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
   import("@fluencelabs/js-client"),
   import("@sinclair/typebox"),
   import("../dist/compiled-aqua/main.js"),
-  import("../dist/relays.json"),
+  import("../dist/relays.json", { with: { type: "json" } }),
 ])();
 
 dotenv.config();
