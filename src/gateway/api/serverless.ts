1c1b41679a1c49bab733ae57cc179a914cce34cd
Akim Mamedov <akim99999999@gmail.com>
2024-02-13 23:51:38 +0700

    deploy pls

--- src/gateway/api/serverless.ts
+++ src/gateway/api/serverless.ts
@@ -2,22 +2,6 @@ import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
 import dotenv from "dotenv";
 
-// Runtime dependencies required for this function. Vercel does import only direct listed dependencies.
-(() => [
-  import("@fluencelabs/js-client"),
-  // This import will fail in runtime
-  () => import("@fluencelabs/marine-worker"),
-  import("@fluencelabs/marine-js"),
-  // This imports will impact loading speed
-  () => import("@fluencelabs/marine-js/dist/marine-js.wasm"),
-  () => import("@fluencelabs/avm/dist/avm.wasm"),
-  import("@fluencelabs/threads"),
-  import("observable-fns"),
-  import("@fluencelabs/avm"),
-  import("@fastify/rate-limit"),
-  import("@sinclair/typebox"),
-])();
-
 dotenv.config();
 
 const server = fastify({
